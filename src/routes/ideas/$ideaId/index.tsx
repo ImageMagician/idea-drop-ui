ff10ba6b17828a7ef186fc7ee3c2e0679deb4e40
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-08-06 16:47:43 -0600

    add /api proxy: src/routes/ideas/$ideaId

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,29 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+const fetchIdea = async (ideaId: string) => {
+  const res = await fetch(`/api/ideas/${ideaId}`);
+  if (!res.ok) throw new Error('Failed to fetch data.');
+  return res.json();
+}
+
 export const Route = createFileRoute('/ideas/$ideaId/')({
   component: IdeaDetailsPage,
+  loader: async ({ params }) => {
+    return fetchIdea(params.ideaId)
+  }
 })
 
 function IdeaDetailsPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>
+  const idea = Route.useLoaderData();
+  return <div className={`max-w-4xl mx-auto py-8`}>
+    <h1 className={`text-2xl font-bold mb-3`}>
+      {idea.title}!
+    </h1>
+    <div className="mb-3 text-sm uppercase">
+      {idea.tags}
+    </div>
+    <p>
+      {idea.description}
+    </p>
+  </div>
 }
