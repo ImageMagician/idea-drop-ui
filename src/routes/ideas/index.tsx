85f8b9a423e8be554dc08af08dd68869087a5d15
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-08-10 15:05:05 -0600

    routes/index.tsx, routes/ideas/index.tsx: replace static card with IdeaCard

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,7 +1,7 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-
 import {fetchIdeas} from "@/api/ideas.ts";
+import IdeaCard from "@/components/IdeaCard.tsx";
 
 const ideasQueryOptions = () => queryOptions({
   queryKey: ['ideas'],
@@ -26,27 +26,11 @@ function IdeasPage() {
   const { data: ideas } = useSuspenseQuery(ideasQueryOptions())
   return <div className={`p-4`}>
     <h1 className="text-2xl font-bold mb-4">Ideas</h1>
-    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       { ideas.map( (idea) => (
-          <li key={ idea.id}
-              className={`border border-gray-300 p-4 rounded shadow bg-white flex flex-col justify-between`}
-          >
-            <div>
-              <h2 className="text-lg font-semibold mb-2">{ idea.title }</h2>
-              <p className="mb-2 text-gray-700">
-                { idea.summary}
-              </p>
-            </div>
-
-            <Link to="/ideas/$ideaId"
-                  params={{ ideaId: idea.id.toString()}}
-                  className={`text-center inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition`}
-            >
-              View Idea
-            </Link>
-          </li>
+          <IdeaCard key={ idea.id } idea={ idea } />
         )
       )}
-    </ul>
+    </div>
   </div>
 }
