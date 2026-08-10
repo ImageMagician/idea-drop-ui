2a9d17f8ce2d19c9b57b79cea1c1b2389fae1170
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-08-10 15:04:42 -0600

    component: IdeaCard.tsx: create

---
+++ src/components/IdeaCard.tsx
@@ -0,0 +1,20 @@
+import type { Idea } from "@/types";
+import {Link} from "@tanstack/react-router";
+import clsx from 'clsx'
+
+const IdeaCard = ({ idea, button = true } : {idea: Idea, button?: boolean}) => {
+    const linkClasses = clsx({
+        'text-blue-600 hover:underline mt-3': !button,
+        'text-center mt-4 inline-block bg-blue-600 hover:bg-blue-700 text-white transition px-4 py-2 w-full rounded': button,
+    })
+    return (
+        <div className="border border-gray-300 rounded-lg shadow p-4 bg-white">
+            <h3 className="text-lg font-bold text-gray-900">{ idea.title }</h3>
+            <p className="text-gray-600 mb-2">{ idea.summary }</p>
+            <Link to="/ideas/$ideaId" params={{ideaId: idea.id.toString()}} className={ linkClasses }>
+                { button ? 'View Idea' : 'Read more →' } </Link>
+        </div>
+    )
+}
+
+export default IdeaCard;
