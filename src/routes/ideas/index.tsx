17824d60640574cbd48616315534a427b7225e05
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-08-10 11:35:35 -0600

    routes/ideas/index.tsx: remove local get function, link to api/ideas.ts

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,12 +1,7 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-import type { Idea } from '@/types'
-import api from '@/lib/axios'
 
-const fetchIdeas = async (): Promise<Idea[]> => {
-  const res = await api.get('/ideas')
-  return res.data
-}
+import {fetchIdeas} from "@/api/ideas.ts";
 
 const ideasQueryOptions = () => queryOptions({
   queryKey: ['ideas'],
