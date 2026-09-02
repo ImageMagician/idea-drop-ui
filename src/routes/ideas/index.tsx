8c7acfe5c6c278c50aafdb84722fc1b7317640b4
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 09:46:31 -0600

    ideas/index.tsx: remove front-end sorting

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -23,8 +23,7 @@ export const Route = createFileRoute('/ideas/')({
 })
 
 function IdeasPage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions())
-  const ideas = [...data].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions())
   return <div className={`p-4`}>
     <h1 className="text-2xl font-bold mb-4">Ideas</h1>
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
