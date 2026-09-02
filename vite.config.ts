caa9a38292ba0cb97f85a23bbd88713239814f68
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2026-09-02 09:26:13 -0600

    vite.config.ts: remove rewrite for api

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ const config = defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
