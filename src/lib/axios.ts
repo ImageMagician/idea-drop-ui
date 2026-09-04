1d36bcbc5b2b1d197a0ea99b07519937d7178389
Mark Larson <mlarson154@gmail.com>
2026-09-04 15:06:05 -0600

    axios.ts: added response.use for expired token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { getStoredAccessToken } from "@/lib/authToken";
+import {getStoredAccessToken, setStoredAccessToken} from "@/lib/authToken";
+import { refreshAccessToken } from "#/api/auth.ts";
 
 const api = axios.create({
     baseURL: '/api',
@@ -9,6 +10,7 @@ const api = axios.create({
     }
 });
 
+// Attach token on refresh
 api.interceptors.request.use((config) => {
     const token = getStoredAccessToken();
     if (token) {
@@ -16,6 +18,31 @@ api.interceptors.request.use((config) => {
     }
 
     return config;
-})
+});
+
+// Refresh token after expired
+api.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response?.status === 401
+            && !originalRequest._retry
+            && !originalRequest.url.includes('/auth/refresh')
+        ) {
+           originalRequest._retry = true;
+
+            try {
+                const { accessToken:newToken } = await refreshAccessToken();
+                setStoredAccessToken(newToken);
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                return api(originalRequest);
+            }
+            catch (error) {
+                console.error("Refresh token failed: ", error);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
