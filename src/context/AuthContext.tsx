4a41709d798e4cc9ad4c2a24ffc6a5d990dd97e0
Mark Larson <mlarson154@gmail.com>
2026-09-04 14:10:36 -0600

    AuthContext.tsx: added refreshAccessToken in useEffect

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { refreshAccessToken } from "#/api/auth.ts";
 
 type AuthContextType = {
     accessToken: string | null;
@@ -16,6 +17,21 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [accessToken, setAccessToken] = useState<string | null>(null);
     const [user, setUser] = useState<AuthContextType['user'] | null>(null);
+
+    useEffect(() => {
+        const loadAuth = async () => {
+            try {
+                const { accessToken:newToken, user } = await refreshAccessToken();
+                setAccessToken(newToken);
+                setUser(user);
+            }
+            catch (err:any) {
+                console.log("Failed to refresh token: ", err);
+            }
+        }
+        loadAuth();
+    }, []);
+
 
     return (
         <AuthContext.Provider value={{accessToken, setAccessToken, user, setUser}}>
